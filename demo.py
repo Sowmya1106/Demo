6201b0df66d0768c0bc25df733bee0a180d2d39f
Sowmya1106 <124231164+Sowmya1106@users.noreply.github.com>
2023-12-29 11:46:37 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="cats")
 st.markdown("## Types of cats")
 st.write("- Persian Cat")
-st.image("./ download.jpg")
+st.image("./download.jpg")
 st.write("- White Cat")
 st.image("./pexels-pixabay-104827.jpg")
 
