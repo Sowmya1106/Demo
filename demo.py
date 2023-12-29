64f513be12fb1f67f3e35f46cb5e33c909781cef
Sowmya1106 <124231164+Sowmya1106@users.noreply.github.com>
2023-12-29 11:33:34 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,6 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="cats")
 st.markdown("## Types of cats")
 st.write("- Persian Cat")
+st.image(image="https://www.google.com/url?sa=i&url=https%3A%2F%2Funsplash.com%2Fimages%2Fanimals%2Fcat&psig=AOvVaw1jl8kz9LU9Hl6J6mOVF5x8&ust=1703914252728000&source=images&cd=vfe&ved=0CBIQjRxqFwoTCKjEo7H1s4MDFQAAAAAdAAAAABAJ", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 st.write("- White Cat")
 st.image(image="https://img.freepik.com/free-photo/cute-domestic-kitten-sits-window-staring-outside-generative-ai_188544-12519.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703635200&semt=ais", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
 
