2ca1813b48c7605051bc33d090e3aac867aa5ada
Sowmya1106 <124231164+Sowmya1106@users.noreply.github.com>
2023-12-29 11:43:26 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(page_title="cats")
 st.markdown("## Types of cats")
 st.write("- Persian Cat")
-st.image("download.jpg")
+st.image("./ download.jpg")
 st.write("- White Cat")
-st.image(image="https://img.freepik.com/free-photo/cute-domestic-kitten-sits-window-staring-outside-generative-ai_188544-12519.jpg?size=626&ext=jpg&ga=GA1.1.1546980028.1703635200&semt=ais", caption=None, width=None, use_column_width=None, clamp=False, channels="RGB", output_format="auto")
+st.image("./ white.jpg")
 
