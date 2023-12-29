872e44939073fe26662deb87d614634afa8c6448
Sowmya1106 <124231164+Sowmya1106@users.noreply.github.com>
2023-12-29 12:11:03 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,11 +3,11 @@ st.set_page_config(page_title="cats")
 st.markdown("## Types of cats")
 col1,col2,col3=st.columns(3)
 with col1:
-st.write("- Persian Cat")
-st.image("./download.jpg",width=100,use_column_width=True)
+  st.write("- Persian Cat")
+  st.image("./download.jpg",width=100,use_column_width=True)
 with col2:
-st.write("- White Cat")
-st.image("./pexels-pixabay-104827.jpg",width=100,use_column_width=True)
-with col3:
-st.write("- Rose")
-st.image("./roses.jpg",width=100,use_column_width=True)
+  st.write("- White Cat")
+  st.image("./pexels-pixabay-104827.jpg",width=100,use_column_width=True)
+with col3: 
+  st.write("- Rose")
+  st.image("./roses.jpg",width=100,use_column_width=True)
