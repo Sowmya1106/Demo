51b18329496421afb51c57db0817b83aa9d4afc5
Sowmya1106 <124231164+Sowmya1106@users.noreply.github.com>
2023-12-29 12:18:36 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -9,6 +9,8 @@ with col1:
 with col2:
   st.write("- White Cat")
   st.image("./pexels-pixabay-104827.jpg",width=100,use_column_width=True)
+  st.write("White cats are cute")
 with col3: 
   st.write("- Rose")
   st.image("./roses.jpg",width=100,use_column_width=True)
+  st.write("Roses are beautiful")
